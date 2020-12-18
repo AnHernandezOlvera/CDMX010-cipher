05dc5e41132dde98a768a9cc0c011fc3c4a52177
An <dsnanahi@gmail.com>
2020-12-17 23:17:24 -0600

    primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,72 @@
 const cipher = {
   // ...
-};
+//Función para cifrar
+encode: function (offset, message) {
+  let offsetU = parseFloat(offset);
+  let messageU = document.getElementById('message').value;
+  let valueNumber = Math.sign(offsetU);
+  let alfabeto =[];//cambio
+  if(messageU==null){
+    throw new TypeError('Valores incorrectos');
+  } 
+    for (let i = 0; i < message.length; i++ ){
+        //Obtiene el código ASCII de cada letra
+        alfabeto[i] = message.charCodeAt(i);
+        // eslint-disable-next-line no-console
+        console.log('Hola');
 
+        //Aplica únicamente a las letras de la A-Z / con el código ASCII del 65 al 90 
+       if (alfabeto[i] >= 65 && alfabeto[i]<=90 && valueNumber===1 && messageU!=null) {
+            //Convierte a cadena el valor ASCII aplicando la fórmula de cipher
+            alfabeto[i] = String.fromCharCode((alfabeto[i]-65+offsetU)%26+65);
+            
+        }  
+        
+        else if (alfabeto[i] >= 65 && alfabeto[i]<=90 && valueNumber===-1){
+          alfabeto[i] = message.charCodeAt(i);
+          offsetU = 26+(offsetU%26);
+          //console.log(offsetU);
+          alfabeto[i] = String.fromCharCode((alfabeto[i]-65+offsetU)%26+65);
+          
+        } /*else if(messageU==='NULL'){
+          messageU=== 'NULL'
+          
+        }*/
+
+            
+        else{
+            //Convierte a cadena el valor ASCII de otras letras o caracteres que se quedan fuera del rango 65-90
+            alfabeto[i] = String.fromCharCode (alfabeto[i])
+            //console.log('Hola error2')
+            
+        }
+        
+        //Modifica el elemento del DOM e imprime sin comas el texto cifrado  
+    }
+    console.log(alfabeto.join(''))
+    return alfabeto.join('')
+  
+  },
+  decode: function (offset, printcipher) {
+    let offsetU = parseFloat(offset);
+    let alfabeto =[];
+    
+      for (let i = 0; i < printcipher.length; i++){
+          //Obtiene el código ASCII de cada letra
+          alfabeto[i] = printcipher.charCodeAt(i);
+          //Aplica únicamente a las letras de la A-Z / con el código ASCII del 65 al 90 
+          if (alfabeto[i] >= 65 && alfabeto[i]<=90) {
+              //Convierte a cadena el valor ASCII aplicando la fórmula de cipher
+              alfabeto[i] = String.fromCharCode((alfabeto[i]+65-offsetU)%26+65);
+          }else{
+              //Convierte a cadena el valor ASCII de otras letras o caracteres que se quedan fuera del rango 65-90
+              alfabeto[i] = String.fromCharCode (alfabeto[i])
+          }
+          //Modifica el elemento del DOM e imprime sin comas el texto cifrado  
+      }
+      console.log(alfabeto.join(''))
+      return alfabeto.join('')
+    }
+  
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import cipher from './cipher.js';
+import cipher from './cipher.js'
+console.log(cipher)
 
-console.log(cipher);
+document.getElementById('buttonEncode').addEventListener("click", function () {
+    console.log('soy el evento click')
+    let messageU = document.getElementById('message').value;
+    let offsetU = document.getElementById('offset').value;
+    const result = cipher.encode(offsetU, messageU)
+    console.log({ result })
+    let printCipher = document.getElementById('printcipher');
+    printCipher.innerHTML = result;
+    console.log('soy'+messageU)
+    console.log('soy'+offsetU)
+});
+document.getElementById('buttonDecode').addEventListener("click", function(){
+    let printcipherU = document.getElementById('printcipher').value;
+    let offsetU = document.getElementById('offset').value;
+    const result = cipher.decode(offsetU, printcipherU)
+    console.log({ result })
+    let printCipher = document.getElementById('printcipher');
+    printCipher.innerHTML = result;
+});
+
+document.getElementById('message').addEventListener("keyup", function (){
+    let messageU = document.getElementById('message').value;
+    document.getElementById('message').value = messageU.toUpperCase();
+});
